b77a67b21808c714a8c441b59ccec4564a3f593c
Abdur Rahman <101791074+C231045@users.noreply.github.com>
2026-03-18 15:55:51 +0600

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,9 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        double num = 2.78;
+        int newNum = (int) num;
+
+        System.out.println(num);
+        System.out.println(newNum);
+    }
+}

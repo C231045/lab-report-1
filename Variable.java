3939ff8506202513ff5e670e6589267ec32351fe
Abdur Rahman <101791074+C231045@users.noreply.github.com>
2026-03-18 15:54:54 +0600

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,7 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int id = 231045;
+        String name = "Abdur Rahman";
+        System.out.println(name + " is " + id ");
+    }
+}

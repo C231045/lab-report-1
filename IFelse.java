7ba599657e8bb56d277bc2b8460499ddb8497206
Abdur Rahman <101791074+C231045@users.noreply.github.com>
2026-03-18 17:42:23 +0600

    Create IFelse.java

---
+++ IFelse.java
@@ -0,0 +1,11 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+        int number = 231045;
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}

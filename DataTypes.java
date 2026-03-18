e37af6056654868d63f5eeb1278d707ae8ed362c
Abdur Rahman <101791074+C231045@users.noreply.github.com>
2026-03-18 15:55:21 +0600

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,10 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 5.5;
+        char c = 'A';
+        boolean d = true;
+
+        System.out.println(a + " " + b + " " + c + " " + d);
+    }
+}

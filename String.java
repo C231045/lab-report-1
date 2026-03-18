92becbbc180076d9c94a969f15df8ff6b6c2ba47
Abdur Rahman <101791074+C231045@users.noreply.github.com>
2026-03-18 15:56:35 +0600

    Create  String.java

---
+++ String.java
@@ -0,0 +1,7 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String text = "Java Programming cse iiuc";
+        System.out.println(text.length());
+        System.out.println(text.toUpperCase());
+    }
+}
